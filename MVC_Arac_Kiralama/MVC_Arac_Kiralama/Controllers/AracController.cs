305c48051e627cca54ed59ad9f8f2062f3b91407
metin.sen <metin.sen@ALS-YG39506-J06>
2017-03-26 17:41:09 +0300

    3

---
+++ MVC_Arac_Kiralama/MVC_Arac_Kiralama/Controllers/AracController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Arac_Kiralama.Controllers
+{
+    public class AracController : Controller
+    {
+        // GET: Arac
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_Arac_Kiralama/MVC_Arac_Kiralama/Controllers/PanelController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Arac_Kiralama.Controllers
+{
+    public class PanelController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
